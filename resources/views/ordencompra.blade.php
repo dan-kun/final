919d4f9addb46e358121059398db1ed792147e92
marielba <marielba.perezl@gmail.com>
2019-03-09 09:38:37 -0400

    primera subida, adelanto hasta marzo marielba

---
+++ resources/views/ordencompra.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@extends('menu')
+
+@section('content')
+    <div class="content">
+        <div class="title m-b-md">
+            <h1 style="text-align:center">Estas en vista de Orden de Compra</h1>
+        </div>
+    </div>
+
+<div class="container">
+    <table id="example" class="table table-striped table-bordered" style="width:90%; margin-left:1%">
+       <thead>
+        <tr>
+            <th>ID</th>
+            <th>Orden de Compra</th>
+            <th>Proveedor</th>
+            <th>Cantidad de Creacion </th>
+            <th>Cantidad Existente</th>
+            <th>Exportar </th>
+        </tr>
+       </thead>
+    </table>
+
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable({
+              "ServerSide": true,
+              "ajax": "{{ url('api/ordencompra') }}",
+              "columns": [
+                {data: 'id'},
+                {data: 'orden_compra'},
+                {data: 'proveedor'},
+                {data: 'cant_creacion'},
+                {data: 'cant_existente'},
+                {data: 'btn'},
+      ],
+      "language": {
+
+            "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json"
+
+        },
+            });
+
+        } );
+    </script>
+</div>
+@endsection
